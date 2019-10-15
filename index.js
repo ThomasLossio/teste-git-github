3886a5d6c2a06ae5a453a82cc55474184732af89
Neylanio Soares <neylaniogomes@gmail.com>
2019-10-15 20:01:06 -0300

    segunda alteracao

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/testee', (req, res) => {
+app.get('/testando', (req, res) => {
   return res.json({ msg: 'Hello world!'})
 });
 
